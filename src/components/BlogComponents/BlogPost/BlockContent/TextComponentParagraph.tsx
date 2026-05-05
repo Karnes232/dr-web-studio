c6f772430d2a5d9ea2504881d2e298e7caf1f9d0
James Karnes <karnes.james@gmail.com>
2026-05-05 19:29:15 -0400

    Refactor heading and paragraph components for improved styling consistency
    - Removed dark mode text color classes from heading and paragraph components to streamline styling.
    - Commented out dark mode media query in globals.css for potential future use.

--- src/components/BlogComponents/BlogPost/BlockContent/TextComponentParagraph.tsx
+++ src/components/BlogComponents/BlogPost/BlockContent/TextComponentParagraph.tsx
@@ -22,7 +22,7 @@ const TextComponentParagraph = ({
         className="flex flex-col justify-center max-w-5xl mx-5 lg:p-2 xl:mx-auto"
       >
         <p
-          className={`${playfairDisplay.className} lg:text-lg text-gray-700 dark:text-white ${ParagraphClassName}`}
+          className={`${playfairDisplay.className} lg:text-lg text-gray-700  ${ParagraphClassName}`}
         >
           {paragraph}
         </p>
